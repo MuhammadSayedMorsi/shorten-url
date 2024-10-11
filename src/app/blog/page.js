e1d79a437cb64cea54ef5341f1422eb493aa3115
muhammad sayed <muhammadsayed1001@yahoo.com>
2024-10-11 12:02:28 +0300

    removed fetch

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,11 +1,15 @@
-async function getData() {
-  const endpoint = "http://localhost:3000/api/posts";
-  const res = await fetch(endpoint);
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+// 'use client'
 
-  return res.json();
+async function getData() {
+  // const endpoint = "http://localhost:3000/api/posts";
+  // const res = await fetch(endpoint);
+  // if (!res.ok) {
+  //   throw new Error("Failed to fetch data");
+  // }
+
+  // return res.json();
+
+  return { items: [] };
 }
 
 export default async function BlogPage() {
